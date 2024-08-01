591e3fd0c854cf4123806722301d2d018e28fcac
Adam Herbert <41672441+adherb@users.noreply.github.com>
2024-08-01 11:21:36 +0300

    Create helloworld.js

---
+++ module1/backend/helloworld.js
@@ -0,0 +1,55 @@
+// console.log("Hello Adam");
+
+// this is the structure of a function
+// function ThisIsTheName(Signature) {
+// Body
+// }
+
+// function sayHello() {
+//   console.log("Hello");
+// }
+
+// sayHello();
+
+// function sum(num1, b) {
+//   console.log(num1 + b);
+// }
+
+// sum(5, 1);
+
+// Returns
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// var result = sum(5, 1);
+
+// console.log(result);
+
+console.log("Hello Danni");
+//Task1
+//Addition
+function additionFunction(a, b) {
+  return a + b;
+}
+console.log(additionFunction(5, 1));
+//subtraction
+function minusFunction(a, b) {
+  return a - b;
+}
+// sum(5, 1);
+console.log(minusFunction(5, 1));
+//division
+function divideFunction(a, b) {
+  return a / b;
+}
+console.log(divideFunction(5, 1));
+//multiply
+function multiplyFunction(a, b) {
+  return a * b;
+}
+console.log(multiplyFunction(5, 2));
+//Task2
+let text1 = "Hello";
+let text2 = "Danni";
+let text3 = text1.concat(text2);
