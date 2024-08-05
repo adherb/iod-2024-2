a736c374cc7485cce04423c9154d20212d245071
Adam Herbert <41672441+adherb@users.noreply.github.com>
2024-08-05 19:00:29 +1000

    module1

--- module1/backend/helloworld.js
+++ module1/backend/helloworld.js
@@ -1,3 +1,16 @@
+// const Sentiment = require("sentiment");
+// const sentiment = new Sentiment();
+
+// function analyzeSentiment(text) {
+//   const result = sentiment.analyze(text);
+//   return result;
+// }
+
+// // Example usage
+// const text = "Cats are stupid.";
+// const analysisResult = analyzeSentiment(text);
+// console.log(analysisResult);
+
 // console.log("Hello Adam");
 
 // this is the structure of a function
@@ -26,30 +39,65 @@
 
 // console.log(result);
 
-console.log("Hello Danni");
-//Task1
-//Addition
-function additionFunction(a, b) {
-  return a + b;
-}
-console.log(additionFunction(5, 1));
-//subtraction
-function minusFunction(a, b) {
-  return a - b;
-}
-// sum(5, 1);
-console.log(minusFunction(5, 1));
-//division
-function divideFunction(a, b) {
-  return a / b;
-}
-console.log(divideFunction(5, 1));
-//multiply
-function multiplyFunction(a, b) {
-  return a * b;
-}
-console.log(multiplyFunction(5, 2));
-//Task2
-let text1 = "Hello";
-let text2 = "Danni";
-let text3 = text1.concat(text2);
+// console.log("Hello Danni");
+// //Task1
+// //Addition
+// function additionFunction(a, b) {
+//   return a + b;
+// }
+// console.log(additionFunction(5, 1));
+// //subtraction
+// function minusFunction(a, b) {
+//   return a - b;
+// }
+// // sum(5, 1);
+// console.log(minusFunction(5, 1));
+// //division
+// function divideFunction(a, b) {
+//   return a / b;
+// }
+// console.log(divideFunction(5, 1));
+// //multiply
+// function multiplyFunction(a, b) {
+//   return a * b;
+// }
+// console.log(multiplyFunction(5, 2));
+// //Task2
+// let text1 = "Hello";
+// let text2 = "Danni";
+// let text3 = text1.concat(text2);
+
+// var name = "Adam";
+
+// console.log(name);
+
+// // console.log(“hello” + name)
+// console.log("this is a Message");
+// console.info("this is info");
+// console.warn("this is a warning");
+// console.error("this is an error");
+
+// const helloText = "say Hi";
+// helloText = "say Hello instead"; // error: Assignment to constant variable
+
+// const helloText = "say Hi";
+// const helloText = "say Hello instead"; // error: Identifier 'helloText' has
+
+// objects in javascript contain keys (or properties) with corresponding values
+const user = {
+  first_name: "Adam",
+  last_name: "Herbert",
+  age: 36,
+  followers: 987,
+};
+
+console.log(user.first_name);
+console.log(user.age);
+
+console.log(user["last_name"]);
+
+user.followers = 1987;
+console.log(user.followers);
+
+user.location = "Gold Coast";
+console.log(user.location);
